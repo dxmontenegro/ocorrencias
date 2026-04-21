0d1e5fd1077bf3151279e0c7ce6e69ec2ac2f545
dxmontenegro <monte.eng@gmail.com>
2026-04-21 19:11:40 -0300

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   try {
 
     const resposta = await fetch(
-      "https://mspucfzsdpejwnhmwwmk.supabase.co/rest/v1/ocorrencia?select=*&limit=1",
+      "https://mspucfzsdpejwnhmwwmk.supabase.co/rest/v1/ocorrencias?select=*&limit=1",
       {
         headers: {
           apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1zcHVjZnpzZHBlanduaG13d21rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA3MjE5NTMsImV4cCI6MjA4NjI5Nzk1M30.2fz71fsG-eAJeKXNYRyUTWHSzwx9OLAVtrVj0Ynxk3s"
